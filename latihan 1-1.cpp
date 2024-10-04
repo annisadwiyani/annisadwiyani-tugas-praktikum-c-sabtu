d900a3b142dd6fd98c5ed57289c0727569a4b977
annisadwiyani <annisadwiyani93@gmail.com>
2024-10-04 13:14:36 +0700

    Add files via upload

---
+++ latihan 1-1.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int luas;
+	float alas = 8.0;
+	float tinggi = 5.0;
+	
+	luas = 0.5 * alas * tinggi;
+	
+	printf("LUAS SEGITIGA");
+	printf("\nLuas = %i", luas);
+	printf("\nALAS = %.f", alas);
+	printf("\nTINGGI = %.f", tinggi);
+	
+	return 0;
+}
