5b8c0b7f3294e1ce0ee1b9a29a119f799c4448de
annisadwiyani <annisadwiyani93@gmail.com>
2024-10-09 18:49:10 +0700

    Add files via upload

---
+++ latihan soal 3.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	float alas, tinggi, sisi_miring;
+	
+	// Input sisi alas dan sisi tinggi
+	printf("Masukan panjang alas segitiga(dalam cm): ");
+	scanf("%f", &alas);
+	
+	printf("Masukan tinggi segitiga (dalam cm): ");
+	scanf("%f", &tinggi)
+	
+	// Menghitung sisi miring menggunakan rumus phythagoras
+	;sisi_miring = sqrt((alas*alas) + (tinggi*tinggi))
+	
+	// Menampilkan hasil perhitungan
+	;printf("Panjang sisi miring segitiga adalah: %.2f cm\n", sisi_miring)
+	
+	;return 0;
+}
